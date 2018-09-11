639e35c87b7ca52c7c0f68898997f949f9006cea
Alex <designdigitalhealth@gmail.com>
2018-09-11 03:01:03 -0400

    Upd

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area col-md-8">
+	<div id="primary" class=" col-md-6"  style="border: 1px solid blue; margin: 0; width: 30%;">
 		<main id="main" class="site-main">
 
 		<?php
@@ -54,9 +54,9 @@ get_header();
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<div class="col-md-4">
-	<?php get_sidebar(); ?>
-</div>
+<!-- <div class="col-md-2" style="border: 1px solid blue;"> --><!-- content-area col-md-2 -->
+	<?php get_sidebar(); 
 
 
-<?php get_footer(); ?>
+get_footer(); 
+?>
--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,11 @@
  * @package Digital_Health_Blog
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+if ( ! is_active_sidebar( 'right-sidebar' ) ) {
 	return;
 }
 ?>
 
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+<aside id="secondary" class="widget-area" style="width: 15%; display: inline-block;">
+	<?php dynamic_sidebar( 'right-sidebar' ); ?>
 </aside><!-- #secondary -->
