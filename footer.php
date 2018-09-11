ed323a10a2b8769c18593b921b9472a15d775f3a
Alex <designdigitalhealth@gmail.com>
2018-09-11 02:17:24 -0400

    Updates - Bootstrapalizing the theme files.

--- footer.php
+++ footer.php
@@ -13,8 +13,8 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
+	<footer id="colophon" class="site-footer row">
+		<div class="site-info col-md-12">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'digital-health-blog' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
@@ -24,13 +24,19 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'digital-health-blog' ), 'digital-health-blog', '<a href="https://portfolio.alexbelinsky.com">Alex Belinsky</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'digital-health-blog' ), 'Digital Health Blog', '<a href="https://portfolio.alexbelinsky.com">Alexandra</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -15,6 +15,8 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
@@ -25,27 +27,45 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'digital-health-blog' ); ?></a>
 
 	<header id="masthead" class="site-header">
-	<nav id="menu" class="navbar navbar-expand-md navbar-light" role="navigation">	
-		<div class="site-branding navbar-brand">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$digital_health_blog_description = get_bloginfo( 'description', 'display' );
-			if ( $digital_health_blog_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $digital_health_blog_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
 
-		<button class="navbar-toggle navbar-toggler-right" type="button" data-toggle="collapse" data-target="#bs4navbar" area-controls="bs4navbar" area-expanded="false" area-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
+		<nav id="menu" class="navbar navbar-expand-md navbar-light" role="navigation">	
+			<div class="site-branding navbar-brand">
+				<?php
+				the_custom_logo();
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+				$digital_health_blog_description = get_bloginfo( 'description', 'display' );
+				if ( $digital_health_blog_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $digital_health_blog_description; /* WPCS: xss ok. */ ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
+
+			<button class="navbar-toggle navbar-toggler-right" type="button" data-toggle="collapse" data-target="#bs4navbar" area-controls="bs4navbar" area-expanded="false" area-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
+			<?php 
+			wp_nav_menu ([
+				'menu' => 'primary',
+				'theme_location' => 'primary',
+				'container' => 'div',
+				'container_id' => 'bs4navbar',
+				'container_class' => 'collapse navbar-collapse',
+				'menu_id' => 'main-menu',
+				'menu_class' => 'navbar-nav ml-auto',
+				'depth' => 2,
+				'fallback_cb' => 'bs4navwalker::fallback',
+				'walker' => new bs4navwalker()
+			]);
+			?>
+		</nav>
 
 
 
