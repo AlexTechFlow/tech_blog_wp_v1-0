1e107d4244da723e5f2d6fd03dbc5eb1a42ab4f7
Alex <designdigitalhealth@gmail.com>
2018-09-10 23:50:49 -0400

    widgets.php: right sidebar

--- inc/widgets.php
+++ inc/widgets.php
@@ -6,8 +6,8 @@
  */
 function digital_health_blog_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'digital-health-blog' ),
-		'id'            => 'sidebar-1',
+		'name'          => esc_html__( 'Right Sidebar', 'digital-health-blog' ),
+		'id'            => 'right-sidebar',
 		'description'   => esc_html__( 'Add widgets here.', 'digital-health-blog' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
